34157936a577bff9f2394b2e10c23e3f7d2ca8d9
Ed McCormic <ed@edmccormic.com>
2021-02-02 21:24:10 -0500

    aem work 0202

---
+++ ui.frontend/src/app/components/image/image.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MapTo } from '@adobe/aem-angular-editable-components';
+
+const ImageEditConfig = {
+    emptyLabel: 'Image',
+    isEmpty: cqModel =>
+        !cqModel || !cqModel.src || cqModel.src.trim().length < 1
+};
+
+@Component({
+    selector: 'app-image',
+    templateUrl: './image.component.html',
+    styleUrls: ['./image.component.css']
+})
+export class ImageComponent implements OnInit {
+
+    @Input() src: string;
+    @Input() alt: string;
+    @Input() title: string;
+
+    constructor() { }
+
+    get hasImage() {
+        return this.src && this.src.trim().length > 0;
+    }
+
+    ngOnInit() { }
+}
+
+MapTo('ecomm-nxtgen-pd-aem/components/image')(ImageComponent, ImageEditConfig);
